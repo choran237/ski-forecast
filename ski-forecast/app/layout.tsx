db5bdc5f63b51aee8b2ed21fb2624061e06de2ef
choran237 <nikhil.choraria@gmail.com>
2026-03-10 13:54:33 +0000

    rebrand: London Powder Hunter

--- ski-forecast/app/layout.tsx
+++ ski-forecast/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ski Forecast Desk",
+  title: "London Powder Hunter",
   description: "Live snow forecasts, ratings & lesson prices for top ski resorts",
 };
 
